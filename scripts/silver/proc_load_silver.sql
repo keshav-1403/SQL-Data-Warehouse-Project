3c0dcf326dea7343d8d9aff9c6a9fbe421baf7f6
Keshav_1403 <keshavupadhyay1403@gmail.com>
2025-03-17 21:27:50 +0530

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -48,23 +48,25 @@ BEGIN
 			cst_create_date
 		)
 		SELECT
-		cst_id,
-		cst_key,
-		TRIM(cst_firstname) AS cst_firstname,
-		TRIM(cst_lastname) AS cst_lastname,
-		CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'SINGLE'
+			cst_id,
+			cst_key,
+			TRIM(cst_firstname) AS cst_firstname,
+			TRIM(cst_lastname) AS cst_lastname,
+			CASE 
+				WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'SINGLE'
 				WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'MARRIED'
 				ELSE 'n/a'
-		END cst_marital_status,
-		CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'FEMALE'
+			END cst_marital_status,
+			CASE 
+				WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'FEMALE'
 				WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'MALE'
 				ELSE 'n/a'
-		END cst_gndr,
-		cst_create_date
+			END cst_gndr,
+			cst_create_date
 		FROM (
 			SELECT 
-					*,
-					ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+				*,
+				ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
 			FROM bronze.crm_cust_info
 			WHERE cst_id IS NULL
 		)t  
@@ -81,14 +83,14 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: silver.crm_prd_info';
 		INSERT INTO silver.crm_prd_info(
-				prd_id,
-				cat_id,
-				prd_key,
-				prd_nm,
-				prd_cost,
-				prd_line,
-				prd_start_dt,
-				prd_end_dt
+			prd_id,
+			cat_id,
+			prd_key,
+			prd_nm,
+			prd_cost,
+			prd_line,
+			prd_start_dt,
+			prd_end_dt
 		)
 		SELECT
 			prd_id,
@@ -97,16 +99,16 @@ BEGIN
 			prd_nm,
 			ISNULL(prd_cost, 0) AS prd_cost,
 			CASE UPPER(TRIM(prd_line))
-					WHEN  'M' THEN 'Mountain'
-					WHEN  'R' THEN 'Road'
-					WHEN  'S' THEN 'Other Sales'
-					WHEN  'T' THEN 'Touring'
-					ELSE 'n/a'
+				WHEN  'M' THEN 'Mountain'
+				WHEN  'R' THEN 'Road'
+				WHEN  'S' THEN 'Other Sales'
+				WHEN  'T' THEN 'Touring'
+				ELSE 'n/a'
 			END AS prd_line,
 			CAST(prd_start_dt AS DATE) AS prd_start_dt,
 			CAST(
-					LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)-1 
-					AS DATE
+				LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)-1 
+				AS DATE
 			) AS prd_end_dt -- Calculate end date as one day before the next start date
 		FROM bronze.crm_prd_info;
         SET @end_time = GETDATE();
@@ -121,43 +123,43 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: silver.crm_sales_details';
 		INSERT INTO silver.crm_sales_details (
-				sls_ord_num,
-				sls_prd_key,
-				sls_cust_id,
-				sls_order_dt,
-				sls_ship_dt,
-				sls_due_dt,			
-				sls_sales,			
-				sls_quantity,		
-				sls_price			
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			sls_order_dt,
+			sls_ship_dt,
+			sls_due_dt,			
+			sls_sales,			
+			sls_quantity,		
+			sls_price			
 		)
 		SELECT 
-				sls_ord_num,
-				sls_prd_key,
-				sls_cust_id,
-				CASE 
-						WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
-						ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
-				END AS sls_order_dt,
-				CASE 
-						WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
-						ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
-				END AS sls_ship_dt,
-				CASE 
-						WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
-						ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
-				END AS sls_due_dt,
-				CASE 
-						WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
-								THEN sls_quantity * ABS(sls_price)
-						ELSE sls_sales
-				END AS sls_sales, -- Recalculate sales if original value is missing or incorrect
-				sls_quantity,
-				CASE 
-						WHEN sls_price IS NULL OR sls_price <= 0
-							THEN sls_sales/NULLIF(sls_quantity, 0)
-						ELSE sls_price
-				END AS sls_price  -- Derive price if original value is invalid	
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			CASE 
+				WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+				ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+			END AS sls_order_dt,
+			CASE 
+				WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+				ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+			END AS sls_ship_dt,
+			CASE 
+				WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+				ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+			END AS sls_due_dt,
+			CASE 
+				WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+						THEN sls_quantity * ABS(sls_price)
+				ELSE sls_sales
+			END AS sls_sales, -- Recalculate sales if original value is missing or incorrect
+			sls_quantity,
+			CASE 
+				WHEN sls_price IS NULL OR sls_price <= 0
+					THEN sls_sales/NULLIF(sls_quantity, 0)
+				ELSE sls_price
+			END AS sls_price  -- Derive price if original value is invalid	
 		FROM bronze.crm_sales_details
 		SET @end_time = GETDATE();
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
@@ -175,23 +177,23 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: silver.erp_cust_az12';
 		INSERT INTO silver.erp_cust_az12 (
-				cid,
-				bdate,
-				gen
+			cid,
+			bdate,
+			gen
 		)
 		SELECT 
 			CASE 
-					WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
-					ELSE cid
+				WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+				ELSE cid
 			END cid,
 			CASE 
-					WHEN bdate > GETDATE() THEN NULL
-					ELSE bdate
+				WHEN bdate > GETDATE() THEN NULL
+				ELSE bdate
 			END AS bdate,  -- Set future birthdates to NULL
 			CASE 
-					WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
-					WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
-					ELSE 'n/a'
+				WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+				WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+				ELSE 'n/a'
 			END AS gen	-- Normalize gender values and handle unknown cases
 		FROM bronze.erp_cust_az12
 	    SET @end_time = GETDATE();
@@ -206,16 +208,16 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: silver.erp_loc_a101';
 		INSERT INTO silver.erp_loc_a101(
-				cid,
-				cntry
+			cid,
+			cntry
 		)
 		SELECT 
 				REPLACE(cid,  '-', '') cid,
 				CASE 
-						WHEN TRIM(cntry) = 'DE' THEN 'Germany'
-						WHEN TRIM(cntry) IN('US', 'USA') THEN 'United States'
-						WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
-						ELSE TRIM(cntry)
+					WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+					WHEN TRIM(cntry) IN('US', 'USA') THEN 'United States'
+					WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+					ELSE TRIM(cntry)
 				END AS cntry -- Normalize and Handle missing or blank country codes
 		FROM bronze.erp_loc_a101	
 		SET @end_time = GETDATE();
@@ -230,16 +232,16 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: silver.erp_px_cat_g1v2';
 		INSERT INTO silver.erp_px_cat_g1v2 (
-				id,
-				cat,
-				subcat,
-				maintenance
+			id,
+			cat,
+			subcat,
+			maintenance
 		)
 		SELECT
-				id,
-				cat,
-				subcat,
-				maintenance
+			id,
+			cat,
+			subcat,
+			maintenance
 		FROM bronze.erp_px_cat_g1v2;
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
